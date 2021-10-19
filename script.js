c83208852d2fe57fd93211aa2ce8590a93dcb0e2
Abhinav Anand <85018022+Super-Abhinav@users.noreply.github.com>
2021-10-19 19:08:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var colors = ["#8b5642", "#6a696b"];
+
+function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < Date.now() + 15000) {
+    requestAnimationFrame(frame);
+  }
+}
+
+window.onload = frame();
+
+var x = document.getElementById("myAudio");
+
+function playSound() {
+	x.play();
+  console.log("play");
+}
